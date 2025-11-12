a982c3cd864ba41921d698d1136efe79554af0c1
zyonson <wakeup5216@gmail.com>
2025-11-13 00:16:36 +0900

    目的地近くの飲食店をAPIから取得するよう変更

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -2,9 +2,11 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"today-go/config"
 )
@@ -18,21 +20,32 @@ type Event struct {
 	DestiNation string `json:"location"`
 }
 
+type DisplayName struct {
+	Text string `json:"text"`
+}
+
 type Place struct {
-	Rating      float64 `json:"rating"`
-	MapUri      string  `json:"googleMapsUri"`
-	StoreSite   string  `json:"websiteUri"`
-	DisplayName struct {
-		Text string `json:"text"`
-	} `json:"displayName"`
+	Rating      float64     `json:"rating"`
+	MapUri      string      `json:"googleMapsUri"`
+	DisplayName DisplayName `json:"displayName"`
 }
 
 type PlacesResponse struct {
 	Places []Place `json:"places"`
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
+type PlacesAPIResponse struct {
+	Results []struct {
+		Name             string  `json:"name"`
+		Rating           float64 `json:"rating"`
+		FormattedAddress string  `json:"formatted_address"`
+		PlaceID          string  `json:"place_id"`
+	} `json:"results"`
+	Status string `json:"status"`
+}
 
+func index(w http.ResponseWriter, r *http.Request) {
+	// イベント情報の読み込み
 	eventFile, err := os.Open("b.json")
 	if err != nil {
 		log.Fatal("ファイルを開けませんでした。", err)
@@ -50,27 +63,50 @@ func index(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	file, err := os.Open("a.json")
-	if err != nil {
-		log.Fatal("ファイルを開けませんでした。", err)
-	}
-	defer file.Close()
-
-	bytes, err := io.ReadAll(file)
-	if err != nil {
-		log.Fatal("ファイルの読み込みに失敗しました。", err)
+	// Google Places APIでお店情報を取得
+	apiKey := config.Config.GoogleMapsAPIKey
+	if apiKey == "" {
+		log.Fatal("APIキーが設定されていません")
 	}
 
-	var places PlacesResponse
-	err = json.Unmarshal(bytes, &places)
+	query := "レストラン " + eventList.Events[0].DestiNation
+	endpoint := "https://maps.googleapis.com/maps/api/place/textsearch/json"
+
+	params := url.Values{}
+	params.Add("query", query)
+	params.Add("key", apiKey)
+	params.Add("language", "ja")
+
+	fullURL := fmt.Sprintf("%s?%s", endpoint, params.Encode())
+
+	resp, err := http.Get(fullURL)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("リクエスト失敗:", err)
+	}
+	defer resp.Body.Close()
+
+	var result PlacesAPIResponse
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		log.Fatal("レスポンスの解析失敗:", err)
 	}
 
+	var places []Place
+	for _, r := range result.Results {
+		places = append(places, Place{
+			Rating: r.Rating,
+			MapUri: "https://www.google.com/maps/place/?q=place_id:" + r.PlaceID,
+			DisplayName: DisplayName{
+				Text: r.Name,
+			},
+		})
+	}
+
+	// テンプレートに渡すデータ
 	data := map[string]interface{}{
 		"eventList": eventList,
-		"places":    places,
-		"apiKey":    config.Config.GoogleMapsAPIKey,
+		"places":    PlacesResponse{Places: places},
+		"apiKey":    apiKey,
 	}
+
 	generateHTML(w, data, "top")
 }
